37f68e8874489576b21bf2cb50d5061ba46f44e4
bragelbytes <85720959+bragelbytes@users.noreply.github.com>
2022-06-13 16:23:24 -0500

    Changed fileURL to configDirectoryURL

--- Sources/CanaryLibraryiOS/CanaryTest.swift
+++ Sources/CanaryLibraryiOS/CanaryTest.swift
@@ -228,47 +228,47 @@ struct CanaryTest
                 return
             }
                 
-            for case let fileURL as URL in fileArray
+            for case let configDirectoryURL as URL in fileArray
             {
-                guard fileURL.startAccessingSecurityScopedResource() else
+                guard configDirectoryURL.startAccessingSecurityScopedResource() else
                 {
                     continue
                 }
 
-                guard let resourceValues = try? fileURL.resourceValues(forKeys: Set<URLResourceKey>(keys)),
+                guard let resourceValues = try? configDirectoryURL.resourceValues(forKeys: Set<URLResourceKey>(keys)),
                     let isDirectory = resourceValues.isDirectory
                 else
                 {
-                    fileURL.stopAccessingSecurityScopedResource()
+                    configDirectoryURL.stopAccessingSecurityScopedResource()
                     continue
                 }
 
                 if isDirectory
                 {
-                    fileURL.stopAccessingSecurityScopedResource()
+                    configDirectoryURL.stopAccessingSecurityScopedResource()
                     continue
                 }
                 
                 for thisTransportName in possibleTransportNames
                 {
-                    let transportTestName = fileURL.deletingPathExtension().lastPathComponent
+                    let transportTestName = configDirectoryURL.deletingPathExtension().lastPathComponent
                     
                     if transportTestName.lowercased().contains(thisTransportName.lowercased())
                     {
-                        if let newTransport = Transport(name: transportTestName, typeString: thisTransportName, configPath: fileURL.path)
+                        if let newTransport = Transport(name: transportTestName, typeString: thisTransportName, configPath: configDirectoryURL.path)
                         {
                             testingTransports.append(newTransport)
                             uiLogger.info("\n✔️ \(newTransport.name) test is ready\n")
                         }
                         else
                         {
-                            uiLogger.error("⚠️ Failed to create a new transport using the provided config at \(fileURL.path)")
+                            uiLogger.error("⚠️ Failed to create a new transport using the provided config at \(configDirectoryURL.path)")
                             continue
                         }
                     }
                 }
                 
-                fileURL.stopAccessingSecurityScopedResource()
+                configDirectoryURL.stopAccessingSecurityScopedResource()
             }
         }
         
